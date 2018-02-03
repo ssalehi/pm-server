8037d805d4eff227001d537d59b3672461153ae6
arhariri <ali.71hariri@gmail.com>
2018-02-03 15:35:00 +0330

    fix bug in configure.js

--- configure.js
+++ configure.js
@@ -2,25 +2,28 @@
  * Created by Eabasir on 31/01/2017.
  */
 const models = require('./mongo/models.mongo');
+const db = require('./mongo/index');
 const _const = require('./lib/const.list');
 
-
-let admin = models['Agent']({
-  username: 'admin@persianmode.com',
-  secret: 'admin@123',
-  access_level: _const.ACCESS_LEVEL.Admin
-});
-
-admin.save()
+db.dbIsReady()
   .then(() => {
-    console.log('-> ', 'default admin has been added!');
-  })
-  .then(() => {
-    process.exit();
-  })
-  .catch(err => {
-    console.log('-> ', err);
-    process.exit();
+    let admin = new models['Agent']({
+      username: 'admin@persianmode.com',
+      secret: 'admin@123',
+      access_level: _const.ACCESS_LEVEL.Admin
+    });
+
+    admin.save()
+      .then(() => {
+        console.log('-> ', 'default admin has been added!');
+      })
+      .then(() => {
+        process.exit();
+      })
+      .catch(err => {
+        console.log('-> ', err);
+        process.exit();
+      });
   });
 
 
