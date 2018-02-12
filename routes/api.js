856d8bcfdeea26b4c6df41709ae73002ed7b9022
Eabasir <eabasir@gmail.com>
2018-02-12 19:27:41 +0330

    complete product color apis

--- routes/api.js
+++ routes/api.js
@@ -149,7 +149,7 @@ router.use('/product/image/:id/:colorId', function (req, res, next) {
   let productStorage = multer.diskStorage({
     destination,
     filename: (req, file, cb) => {
-      cb(null, [file.originalname, file.mimetype.substr(file.mimetype.lastIndexOf('/') + 1)].join('.'));
+      cb(null, file.originalname);
     }
   });
   let productUpload = multer({storage: productStorage});
