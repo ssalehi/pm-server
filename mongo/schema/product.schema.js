2596d8600a2cabb1fcbad716c6f310feb7312a66
samanvaziri <saman.fv@gmail.com>
2018-03-29 17:29:08 +0430

    product details and errors

--- mongo/schema/product.schema.js
+++ mongo/schema/product.schema.js
@@ -75,7 +75,7 @@ let schema_obj = {
     default: Date.now
   },
   desc: String,
-  details: Schema.Types.string,
+  details: Schema.Types.String,
   tags: [nestedTagSchema],
   reviews: [ProductReview],
   colors: [ProductColor],
