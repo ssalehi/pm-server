9a86ae460a8fa9f83d5e3409cbfa871a3053b156
Eabasir <eabasir@gmail.com>
2018-04-04 14:38:23 +0330

    add ticket schema to order model

--- mongo/schema/ticket.schema.js
+++ mongo/schema/ticket.schema.js
@@ -1,9 +1,10 @@
-const Schema = require('mongoose').Schema;
+const Schema = require('m ongoose').Schema;
 let schema_obj = {
   warehouse_id: Schema.Types.ObjectId,
   status: Number,
   desc: String,
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
+  is_processed: {type: Boolean, default: false}
 };
 
 
