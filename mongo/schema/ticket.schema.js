6395a3135d7a1a0e52a921984e272eb5b41bb24e
samanvaziri <saman.fv@gmail.com>
2018-04-19 11:45:27 +0430

    Merge branch 'master' of https://github.com/PersianMode/server into client-panel
    # Conflicts:
    #	lib/const.list.js
    #	lib/errors.list.js
    #	lib/order.model.js

--- mongo/schema/ticket.schema.js
+++ mongo/schema/ticket.schema.js
@@ -3,11 +3,16 @@ let schema_obj = {
   warehouse_id: Schema.Types.ObjectId,
   status: Number,
   desc: String,
-  timestamp: { type: Date, default: Date.now },
-  is_processed: {type: Boolean, default: false}
+  timestamp: {type: Date, default: Date.now},
+  is_processed: {type: Boolean, default: false, required: true},
+  referral_advice: {type: Number},
+  agent_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Agent'
+  }
 };
 
 
 let TicketSchema = new Schema(schema_obj, {strict: true});
 
-module.exports = TicketSchema ;
+module.exports = TicketSchema;
