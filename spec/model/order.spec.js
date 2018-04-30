0a8338d5301e61648d6facde757bfe7428afdc73
Farhad Mohammadali <mafphp@gmail.com>
2018-04-30 17:41:56 +0430

    002 - shop | center

--- spec/model/order.spec.js
+++ spec/model/order.spec.js
@@ -6,7 +6,7 @@ const _const = require('../../lib/const.list');
 const Order = require('../../lib/order.model');
 const rp = require('request-promise');
 
-xdescribe('POST Order - verify order', () => {
+describe('POST Order - verify order', () => {
 
   let customer1 = {
     cid: null,
@@ -356,19 +356,19 @@ describe('POST Order - ORP', () => {
               price: 4000,
               barcode: '19231213123',
               inventory: [{
-                count: 1,
-                reserved: 0,
-                warehouse_id: _warehouses[0]._id
-              },{
                 count: 2,
-                reserved: 0,
-                warehouse_id: _warehouses[1]._id
+                reserved: 2,
+                warehouse_id: _warehouses[0]._id
               },{
                 count: 3,
                 reserved: 0,
-                warehouse_id: _warehouses[2]._id
+                warehouse_id: _warehouses[1]._id
               },{
                 count: 4,
+                reserved: 0,
+                warehouse_id: _warehouses[2]._id
+              },{
+                count: 5,
                 reserved: 0,
                 warehouse_id: _warehouses[3]._id
               }]
@@ -397,12 +397,28 @@ describe('POST Order - ORP', () => {
             product_instance_id: productInstanceIds[1],
             tickets: []
           }]
+        },{
+          customer_id: customer1.cid,
+          total_amount: 2,
+          order_time: new Date(),
+          is_cart: false,
+          address: warehouses[1].address,
+          transaction_id: mongoose.Types.ObjectId(),
+          order_lines: [{
+            product_id: productIds[0],
+            product_instance_id: productInstanceIds[0],
+            tickets: []
+          }, {
+            product_id: productIds[0],
+            product_instance_id: productInstanceIds[1],
+            tickets: []
+          }]
         }];
 
         return models['OrderTest'].insertMany(orders);
       })
       .then(res => {
-        _order = res[0]
+        _order = res;
         orderIds = res.map(x => x._id);
         done();
       })
@@ -412,37 +428,74 @@ describe('POST Order - ORP', () => {
       })
   });
 
-  it('expect should be set ticket for instance that warhouse is center', function (done) {
+  it('expect should be set ticket for instance if warehouse have enough count and is_collect is false', function (done) {
     this.done = done;
     rp({
       method: 'POST',
       uri: lib.helpers.apiTestURL(`checkout`),
       body: {
-        order_id: _order.id,
-        cartItems: _order.order_lines[0],
-        address: _order.address,
-        transaction_id: _order.transaction_id,
-        used_point: _order.used_point,
-        used_balance: _order.used_balance,
-        total_amount: _order.total_amount,
-        is_collect: _order.is_collect
+        order_id: _order[0].id,
+        cartItems: _order[0].order_lines[0],
+        address: _order[0].address,
+        transaction_id: _order[0].transaction_id,
+        used_point: _order[0].used_point,
+        used_balance: _order[0].used_balance,
+        total_amount: _order[0].total_amount,
+        is_collect: _order[0].is_collect
       },
       json: true,
       resolveWithFullResponse: true,
       jar: customer1.jar
     }).then((res) => {
       expect(res.statusCode).toBe(200);
-      return models['OrderTest'].findById(_order._id);
+      return models['OrderTest'].findById(_order[0]._id);
     }).then(res => {
-      expect(res._id).toEqual(_order._id);
-      expect(res.order_lines[0]._id).toEqual(_order.order_lines[0]._id);
+      expect(res._id).toEqual(_order[0]._id);
+      expect(res.order_lines[0]._id).toEqual(_order[0].order_lines[0]._id);
       expect(res.order_lines[0].tickets.length).toBe(1);
+      expect(res.order_lines[0].tickets[0].status).toBe(1);
       
       return models['ProductTest'].findById(productIds[0]).lean();
     }).then(res => {
       let _instanceFind = res.instances.find(x => x._id.toString() === productInstanceIds[0].toString());
       expect(_warehouses[0]._id.toString()).toEqual(_instanceFind.inventory[0].warehouse_id.toString());
       expect(_instanceFind.inventory[0].reserved).toBe(1);
+      done();
+    }).catch(lib.helpers.errorHandler.bind(this));
+  });
+
+  it('expect should be set ticket for instance if warehouse have enough count and is_collect is true', function (done) {
+    this.done = done;
+    rp({
+      method: 'POST',
+      uri: lib.helpers.apiTestURL(`checkout`),
+      body: {
+        order_id: _order[1].id,
+        cartItems: _order[1].order_lines[0],
+        address: _order[0].address,
+        transaction_id: _order[1].transaction_id,
+        used_point: _order[1].used_point,
+        used_balance: _order[1].used_balance,
+        total_amount: _order[1].total_amount,
+        is_collect: _order[1].is_collect
+      },
+      json: true,
+      resolveWithFullResponse: true,
+      jar: customer1.jar
+    }).then((res) => {
+      expect(res.statusCode).toBe(200);
+      return models['OrderTest'].findById(_order[1]._id);
+    }).then((res) => {
+      expect(res._id).toEqual(_order[1]._id);
+      expect(res.order_lines[0]._id).toEqual(_order[1].order_lines[0]._id);
+      expect(res.order_lines[0].tickets.length).toBe(1);
+      expect(res.order_lines[0].tickets[0].status).toBe(1);
+      return models['ProductTest'].findById(productIds[0]).lean();
+    }).then((res) => {
+      let _instanceFind = res.instances.find(x => x._id.toString() === productInstanceIds[1].toString());
+      expect(_warehouses[1]._id.toString()).toEqual(_instanceFind.inventory[1].warehouse_id.toString());
+      expect(_instanceFind.inventory[0].reserved).toBe(2);
+      expect(_instanceFind.inventory[1].reserved).toBe(1);
       done();
     }).catch(lib.helpers.errorHandler.bind(this));
   });
