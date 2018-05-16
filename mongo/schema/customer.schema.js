4a5df5f8942e62ace3b4a5a84aba8fd4db6f4500
Farhad Mohammadali <mafphp@gmail.com>
2018-05-16 16:11:47 +0430

    001 - update schema customer | added preferences

--- mongo/schema/customer.schema.js
+++ mongo/schema/customer.schema.js
@@ -61,6 +61,15 @@ let schema_obj = {
   // wish_list: [Schema.Types.ObjectId],
   wish_list: [wishListItemSchema],
   preferred_tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}],
+  preferences: {
+    size: {
+      type: String,
+    },
+    brand: {
+      type: Schema.Types.ObjectId,
+      ref: 'Brand'
+    },
+  },
   orders: [{type: Schema.Types.ObjectId, ref: 'Order'}],
   addresses: [addressSchema],
   active: { // weather user can system or not
