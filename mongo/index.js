4914e238e489754b5bfb17eb0b3274f1deb3ec01
arhariri <ali.71hariri@gmail.com>
2018-07-08 10:18:12 +0430

    Merge with master branch

--- mongo/index.js
+++ mongo/index.js
@@ -2,53 +2,29 @@ const env = require('../env');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-if (env.isDev) {
-  // mongoose.set('debug', true);
+let testConnection, prodConnection;
 
+prodConnection = mongoose.createConnection(env.db_uri);
+
+if (env.isDev) {
+  testConnection = mongoose.createConnection(env.db_uri_test);
+  // mongoose.set('debug', true);
 }
 
 let dbIsReady = () => {
 
   let testDb = new Promise((resolve, reject) => {
-
-    if (env.isDev) {
-
-      function connect() {
-       const testConnection = mongoose.createConnection(env.db_uri_test);
-        testConnection.on('connected', function () {
-          console.log('-> ', 'Mongoose test has been connected!');
-          resolve({testConnection});
-        });
-        testConnection.on('error', function (err) {
-          console.log('-> ', 'test connection error trying to reconnect ...');
-          setTimeout(connect, 1000);
-
-        });
-      }
-      connect();
-
-    } else
+    testConnection.on('connected', function () {
+      console.log('-> ', 'Mongoose test has been connected!');
       resolve();
+    });
   });
 
-
   let prodDb = new Promise((resolve, reject) => {
-
-    function connect() {
-      const prodConnection = mongoose.createConnection(env.db_uri);
-      prodConnection.on('connected', function () {
-        console.log('-> ', 'Mongoose product has been connected!');
-        resolve({prodConnection});
-      });
-      prodConnection.on('error', function (err) {
-        console.log('-> ', 'prod connection error trying to reconnect ...');
-        setTimeout(connect, 1000);
-
-      });
-    }
-
-    connect();
-
+    prodConnection.on('connected', function () {
+      console.log('-> ', 'Mongoose product has been connected!');
+      resolve();
+    });
   });
 
   return Promise.all([testDb, prodDb])
@@ -57,5 +33,7 @@ let dbIsReady = () => {
 
 
 module.exports = {
-  dbIsReady
+  prodConnection,
+  testConnection,
+  dbIsReady,
 };
