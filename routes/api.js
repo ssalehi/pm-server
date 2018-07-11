6cb1fa7e23b6ce4e3ff3cfadb40e15ff85e633bc
305-2 <sareh@bentoak.systems>
2018-07-11 11:08:16 +0430

    remove products with not compelete images from data base(remove colors has no picture and their instances too)

--- routes/api.js
+++ routes/api.js
@@ -443,6 +443,6 @@ router.delete('/deliveryduration/delete/:id', apiResponse('DeliveryDurationInfo'
 // Customer Delivery Selected
 router.post('/calculate/order/price', apiResponse('DeliveryDurationInfo', 'calculateFinalPrice', false, ['body'])); // body included customer id and delivery_duration id
 
-router.get('/filter/products', apiResponse('Product', 'filterProdcutCollection', false, []));
+router.get('/filter/products', apiResponse('Product', 'filterProductCollection', false, []));
 
 module.exports = router;
