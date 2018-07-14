aed76b99dfbd74b8f7ad02b7099f2e5905066daa
eabasir@gmail.com <eabasir@gmail.com>
2018-07-14 18:09:59 +0330

    remove image resizing

--- images-handler.js
+++ images-handler.js
@@ -106,16 +106,16 @@ main = async () => {
                         const imageDest = path.join(BASE_DEST, product._id.toString(), color._id.toString(), image);
 
                         try {
-                          // if (k === 0) {
-                          //   await imageResizing(imageOrig, imageDest)
-                          //   fs.createReadStream(imageOrig).pipe(fs.createWriteStream(imageDest));
-                          //   await updateProductImages(product._id, color._id, image, true);
+                          if (k === 0) {
+                            // await imageResizing(imageOrig, imageDest)
+                            fs.createReadStream(imageOrig).pipe(fs.createWriteStream(imageDest));
+                            await updateProductImages(product._id, color._id, image, true);
 
-                          // } else {
+                          } else {
                             fs.createReadStream(imageOrig).pipe(fs.createWriteStream(imageDest));
                             await updateProductImages(product._id, color._id, image, false);
 
-                          // }
+                          }
                           console.log('-> ', `${image} is succesfuly added to path: ${path.join(product._id.toString(), color._id.toString())} ${k === 0 ? 'as thumbnail' : ''} `);
                         } catch (err) {
                           console.log('-> ', `error on copying file ${image} from temp folder to destination ${k === 0 ? 'as thumbnail' : ''}`);
