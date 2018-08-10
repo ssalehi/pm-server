be2d5a6f074d27c1cbdb1f174751503d98319df2
eabasir@gmail.com <eabasir@gmail.com>
2018-08-10 20:56:38 +0330

    add downloader shell

--- images-operators/downloader.sh
+++ images-operators/downloader.sh
@@ -1,1 +1,21 @@
-fsfdffdfddfd
+#!/bin/bash
+
+DATE=`date '+%Y-%m-%d'`
+cd ../../
+mkdir -p Images/Archive/${DATE}
+mkdir -p Images/storage
+cd Images
+wget --save-cookies cookies.txt --keep-session-cookies $1
+wget --load-cookies cookies.txt -O chunk.zip $2
+
+unzip chunk.zip -d ./chunk
+rm chunk.zip
+cp -R chunk/*/* Archive/${DATE}/
+echo start conversion of png images to jpeg
+find ./chunk -name "*.png" -exec mogrify -format jpeg {} \;
+find ./chunk -name "*.png" -exec rm {} \;
+find ./chunk -type f -iname '*.jpeg' -exec jpegoptim size=200k {} +
+echo transfering images to temp directory
+cp -R chunk/*/* ../server/public/temp/
+rm cookies.txt Ef*
+rm -r chunk
