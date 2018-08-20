725d5bacf303bb3fe4e9bf4243d15d419d3423c7
eabasir@gmail.com <eabasir@gmail.com>
2018-08-20 13:02:48 +0330

    c

--- socket/index.js
+++ socket/index.js
@@ -41,7 +41,7 @@ let setup = http => {
   io.on('connection', socket => {
     console.log('-> ', 'connection made...');
 
-    console.log('----> socket ', JSON.stringify(socket, null, 2) );
+    console.log('----> socket ', socket );
     if (socket.session.passport) {
       let user = socket.session.passport.user;
       if (user && user.warehouse_id) {
