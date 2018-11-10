0290cd4c21edfcd3b6eea94fe9da1ba02f9dec97
Qazal <jalilian.gh@gmail.com>
2018-11-10 17:39:50 +0330

    test done

--- spec/api/warehouse/put.spec.js
+++ spec/api/warehouse/put.spec.js
@@ -3,67 +3,93 @@ const lib = require('../../../lib/index');
 const models = require('../../../mongo/models.mongo');
 const error = require('../../../lib/errors.list');
 const mongoose = require('mongoose');
+const warehouses = require('../../../warehouses');
 
 describe('Warehouse PUT API', () => {
     beforeEach(done => {
-      lib.dbHelpers.dropAll()
-        .then(res => {
-          done();
-        })
-        .catch(err => {
-          console.error('Error in beforeEach block: ', err);
-          done();
-        });
+        lib.dbHelpers.dropAll()
+            .then(res => {
+                return models()['WarehouseTest'].insertMany(warehouses);
+                //   done();
+            })
+            .then(res => {
+                done();
+            })
+            .catch(err => {
+                console.error('Error in beforeEach block: ', err);
+                done();
+            });
     });
-  
+
     it('should change priorities', function (done) {
-      this.done = done;
-      rp({
-        method: 'put',
-        body: {
-            "warehouses":[{
-                "_id": "5bd6cde54682480cd8520032",
-                "priority": 2,
-                "is_active":false
-            },
-            {
-                "_id" : "5bd6cde54682480cd8520030",
-             "priority":0,
-             "is_active":false
-            },
-            {
-                "_id" : "5bd6cde54682480cd8520031",
-                "priority":3,
-                "is_active":false
-            },{
-                "_id" : "5bd6cde54682480cd8520033",
-                "priority":1,
-                "is_active":true
-            },
-            {
-                "_id" : "5bd6cde54682480cd8520034",
-                "priority":4,
-                "is_active":false
-            }
-            ]
-        },
-        json: true,
-        uri: lib.helpers.apiTestURL('warehouse/update'),
-        resolveWithFullResponse: true,
-      })
-        .then(res => {
-          expect(res.statusCode).toBe(200);
-         
+        this.done = done;
+        properties = [{
+                    "_id": warehouses[0]._id,
+                    "priority": 2,
+                    "is_active": false
+                },
+                {
+                    "_id": warehouses[1]._id,
+                    "priority": 0,
+                    "is_active": false
+                },
+                {
+                    "_id": warehouses[2]._id,
+                    "priority": 3,
+                    "is_active": false
+                }, {
+                    "_id": warehouses[3]._id,
+                    "priority": 1,
+                    "is_active": true
+                },
+                {
+                    "_id": warehouses[4]._id,
+                    "priority": 4,
+                    "is_active": false
+                }
+            ],
+            rp({
+                method: 'PUT',
 
-        let result = JSON.parse(res.body);  
-        expect(result.length).toBe(5);
-        //   console.log(result)
+                body: {
+                    "warehouses": properties
+                },
 
-        // result.forEach(r => {
-        //   expect(warehouseIds.includes(r._id.toString())).toBeTruthy();
-        // })
-          done();
-        })
-        .catch(lib.helpers.errorHandler.bind(this));
+                json: true,
+                uri: lib.helpers.apiTestURL('warehouse/update'),
+                resolveWithFullResponse: true,
+            })
+
+            .then(res => {
+                expect(res.statusCode).toBe(200);
+                return models()['WarehouseTest'].find();
+            }).then(res => {
+                console.log('->', res);
+
+                expect(res.length).toBe(5);
+
+                // console.log( "properties", properties)
+                // res.forEach(element => {
+                //     const result=  properties.filter(o => !res.find(element => o.id === element.id))
+                //     // const result = properties.filter(item=>{item._id == element._id} );
+                //     console.log("results are:",result);
+                //     expect(element.priority).toBe(result[0].priority)
+                //     expect(element.is_active).toBe(result[0].is_active)
+                // });
+
+                expect(res[0].priority).toBe(2)
+                expect(res[0].is_active).toBe(false)
+                expect(res[1].priority).toBe(0)
+                expect(res[1].is_active).toBe(false)
+                expect(res[2].priority).toBe(3)
+                expect(res[2].is_active).toBe(false)
+                expect(res[3].priority).toBe(1)
+                expect(res[3].is_active).toBe(true)
+                expect(res[4].priority).toBe(4)
+                expect(res[4].is_active).toBe(false)
+
+                done();
+            })
+            .catch(lib.helpers.errorHandler.bind(this));
     });
 })
