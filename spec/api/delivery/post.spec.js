64449f7990e04f2f6463defbf1150a7fe65cb991
arhariri <ali.71hariri@gmail.com>
2018-11-10 10:18:07 +0330

    inner commits

--- spec/api/delivery/post.spec.js
+++ spec/api/delivery/post.spec.js
@@ -1,3 +1,4 @@
+const rp = require('request-promise');
 const lib = require('../../../lib');
 const _const = require('../../../lib/const.list');
 const errors = require('../../../lib/errors.list');
@@ -6,137 +7,111 @@ const models = require('../../../mongo/models.mongo');
 const mongoose = require('mongoose');
 
 describe("Delivery POST API", () => {
-  let salesManager, shopClerk1, shopClerk2;
-  let agentList = [{
-    _id: mongoose.Types.ObjectId(),
-    username: 'isc',
-    secret: 'abc123',
-    access_level: 2,
-    first_name: 'IranMall',
-    surname: 'sc',
-    active: true,
-  }, {
-    _id: mongoose.Types.ObjectId(),
-    username: 'hub',
-    secret: 'abc123',
-    access_level: 3,
-    first_name: 'Mr Hub',
-    surname: 'Clerk',
-    active: true,
-  }, {
-    _id: mongoose.Types.ObjectId(),
-    username: 'deli1',
-    secret: 'abc123',
-    access_level: 4,
-    first_name: 'Mr Delivery',
-    surname: 'Agent',
-    active: true,
-  }, {
-    username: 'deli2',
-    secret: 'abc123',
-    access_level: 4,
-    first_name: 'Ms Delivery',
-    surname: 'Agent',
-    active: true,
-  }];
-  const orderIds = [mongoose.Types.ObjectId(), mongoose.Types.ObjectId(), mongoose.Types.ObjectId()];
-  let deliveryItems = [];
+  let deliveryAgents = [], deliveries = [];
 
   beforeEach(done => {
     lib.dbHelpers.dropAll()
       .then(() => models()['WarehouseTest'].insertMany(warehouses))
-      .then(() => lib.dbHelpers.addAndLoginAgent('sm', _const.ACCESS_LEVEL.SalesManager, warehouses.find(el => !el.is_hub && !el.has_customer_pickup)))
+      .then(() => lib.dbHelpers.addAndLoginAgent('da1', _const.ACCESS_LEVEL.DeliveryAgent))
       .then(res => {
-        salesManager = res;
-        salesManager._id = res.aid;
-        return lib.dbHelpers.addAndLoginAgent('sc', _const.ACCESS_LEVEL.ShopClerk, warehouses.find(el => el.has_customer_pickup && name === 'پالادیوم'));
+        deliveryAgents.push(res);
+        return lib.dbHelpers.addAndLoginAgent('da2', _const.ACCESS_LEVEL.DeliveryAgent);
       })
       .then(res => {
-        shopClerk1 = res;
-        shopClerk1._id = res.aid;
-        return lib.dbHelpers.addAndLoginAgent('sc', _const.ACCESS_LEVEL.ShopClerk, warehouses.find(el => el.has_customer_pickup && name === 'سانا'));
-      })
-      .then(res => {
-        shopClerk2 = res;
-        shopClerk2._id = res.aid;
+        deliveryAgents.push(res);
 
         const customerId = mongoose.Types.ObjectId();
         const addressId = mongoose.Types.ObjectId();
+        const hubId = warehouses.find(el => el.is_hub)._id;
+        const orderIds = [mongoose.Types.ObjectId(), mongoose.Types.ObjectId(), mongoose.Types.ObjectId()];
 
-        deliveryItems = [{
-          order_id: orderIds[0],
-          order_line_id: mongoose.Types.ObjectId(),
-          send: {
-            customer: {
-              id: customerId,
-              address_id: addressId,
-            }
-          },
-          processed_by: salesManager._id,
-          sender: agentList[2]._id,
-          start_date: Date(2010, 10, 10),
-          end_date: Date(2010, 10, 15)
-        }, {
-          order_id: orderIds[0],
-          order_line_id: mongoose.Types.ObjectId(),
-          send: {
-            customer: {
-              id: customerId,
-              address_id: addressId,
-            }
-          },
-          processed_by: agentList[1]._id,
-          sender: agentList[3]._id,
-          start_date: Date(2010, 10, 10),
-          end_date: Date(2010, 10, 15)
-        }, {
-          order_id: orderIds[1],
-          order_line_id: mongoose.Types.ObjectId(),
-          send: {
-            customer: {
-              id: customerId,
-              address_id: addressId,
-            }
-          },
-          processed_by: salesManager._id,
-          sender: agentList[2]._id,
-          start_date: Date(2010, 10, 10),
-          end_date: Date(2010, 10, 15)
-        }, {
-          order_id: orderIds[0],
-          order_line_id: mongoose.Types.ObjectId(),
-          return: {
-            warehouse_id: warehouses[0]._id,
-          },
-          processed_by: agentList[1]._id,
-          sender: agentList[2]._id,
-          start_date: Date(2010, 11, 10),
-          end_date: Date(2010, 11, 15)
-        }, {
-          order_id: orderIds[2],
-          order_line_id: mongoose.Types.ObjectId(),
-          send: {
-            customer: {
-              id: customerId,
-              address_id: addressId,
-            }
-          },
-          processed_by: shopClerk2._id,
-          sender: agentList[3]._id,
-          start_date: new Date(),
-        }, {
-          order_id: orderIds[2],
-          order_line_id: mongoose.Types.ObjectId(),
-          send: {
-            customer: {
-              id: customerId,
-              address_id: addressId,
+        deliveries = [
+          {
+            order_id: orderIds[0],
+            order_line_id: mongoose.Types.ObjectId(),
+            from: {
+              warehouse_id: hubId
             },
+            to: {
+              customer: {
+                id: customerId,
+                address_id: addressId,
+              }
+            },
+            processed_by: salesManager._id,
+            start: Date(2010, 10, 10),
+            end: Date(2010, 10, 15)
           },
-          processed_by: shopClerk2._id,
-        }];
+          {
+            order_id: orderIds[0],
+            order_line_id: mongoose.Types.ObjectId(),
+            from: {
+              warehouse_id: hubId
+            },
+            to: {
+              customer: {
+                id: customerId,
+                address_id: addressId,
+              }
+            },
+            processed_by: agentList[1]._id,
+            start: Date(2010, 10, 10),
+            end: Date(2010, 10, 15)
+          },
+          {
+            order_id: orderIds[1],
+            order_line_id: mongoose.Types.ObjectId(),
+            from: {
+              warehouse_id: hubId
+            },
+            to: {
+              customer: {
+                id: customerId,
+                address_id: addressId,
+              }
+            },
+            processed_by: salesManager._id,
+            delivery_agent: deliveryAgents[0].aid,
+            start: Date(2010, 10, 10),
+            end: Date(2010, 10, 15)
+          },
+          {
+            order_id: orderIds[0],
+            order_line_id: mongoose.Types.ObjectId(),
+            from: {
+              customer: {
+                id: customerId,
+                address_id: addressId,
+              }
+            },
+            to: {
+              warehouse_id: hubId
+            },
+            is_return: true,
+            processed_by: agentList[1]._id,
+            start: Date(2010, 11, 10),
+            end: Date(2010, 11, 15)
+          },
+          {
+            order_id: orderIds[2],
+            order_line_id: mongoose.Types.ObjectId(),
+            from: {
+              customer: {
+                id: customerId,
+                address_id: addressId,
+              }
+            },
+            to: {
+              warehouse_id: hubId
+            },
+            is_return: true,
+            processed_by: shopClerk2._id,
+            delivery_agent: deliveryAgents[0]._id,
+            start: new Date(),
+          },
+        ];
 
-        return models()['DeliveryTest'].insertMany(deliveryItems);
+        return models()['DeliveryTest'].insertMany(deliveries);
       })
       .then(res => {
         done();
@@ -147,23 +122,76 @@ describe("Delivery POST API", () => {
       });
   });
 
-  it("Palladium's shop clerk should get list of related delivery items", function (done) {
+  it("Delivery agent should choose (and assign to himself) multiple delivery items", function (done) {
+    this.done = done;
 
+    rp({
+      method: 'post',
+      uri: lib.helpers.apiTestURL(`/delivery/assign`),
+      body: {
+        delivery_ids: [deliveries[1]._id, deliveries[3]._id]
+      },
+      jar: deliveryAgents[1].rpJar,
+      json: true,
+      resolveWithFullResponse: true,
+    })
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        return models()['DeliveryTest'].find({
+          delivery_agent: deliveryAgents[1]._id
+        });
+      })
+      .then(res => {
+        expect(res.length).toBe();
+        
+        done();
+      })
+      .catch(lib.helpers.errorHandler.bind(this));
   });
 
-  it("Sales manager should get list of related delivery items", function (done) {
+  it("Should get nothing when passed id already was assigned to current delivery agent (not another perosn)", function(done) {
+    rp({
+      method: 'post',
+      uri: lib.helpers.apiTestURL(`/delivery/assign`),
+      body: {
+        delivery_ids: [deliveries[0]._id, deliveries[1]._id, deliveries[2]._id]
+      },
+      jar: deliveryAgents[0].rpJar,
+      json: true,
+      resolveWithFullResponse: true
+    })
+    .then(res => {
+      expect(res.statusCode).toBe(200);
+      return models()['DeliveryAgent'].find({
+        delivery_agent: deliveryAgents[0]._id,
+      });
+    })
+    .then(res => {
 
+      done();
+    })
+    .catch(lib.helpers.errorHandler.bind(this));
   });
 
-  it("Sana's shop clerk should get list of related delivery items", function (done) {
-
+  it("Should get error when passed id already was assigned to another delivery agent", function (done) {
+    rp({
+      method: 'post',
+      uri: lib.helpers.apiTestURL(`/delivery/assign`),
+      body: {
+        delivery_ids: [deliveries[0]._id, deliveries[1]._id, deliveries[2]._id]
+      },
+      jar: deliveryAgents[1].rpJar,
+      json: true,
+      resolveWithFullResponse: true
+    })
+      .then(res => {
+        this.fail('Delivery agent can assign already assigned deliveries');
+        done();
+      })
+      .catch(err => {
+        expect(err.statusCode).toBe();
+        expect(err.error).toBe();
+        done();
+      });
   });
-
-  it("Sales manager should update the delivery details", function (done) {
-
-  });
-
-  it("Palladium's shop clerk should assign delivery agent, date and time for delivery item", function (done) {
-
-  });
-})
+});
