4ec17a3d4874d2796cac9feb78316a51921d39ae
Qazal <jalilian.gh@gmail.com>
2018-11-14 12:09:44 +0330

    commit

--- spec/api/warehouse/put.spec.js
+++ spec/api/warehouse/put.spec.js
@@ -23,40 +23,39 @@ describe('Warehouse PUT API', () => {
 
             .then(res => {
                 adminObj.jar = res.rpJar;
-                console.log(res.rpJar);
-            done()
+             done()
             })
             .catch(err => {
                 console.error('Error in beforeEach block: ', err);
                 done();
             });
-    });
+    }, 15000);
 
     it('should change priorities', function (done) {
         this.done = done;
         properties = [{
-                    "_id": warehouses[0]._id,
-                    "priority": 2,
-                    "is_active": false
+                    _id: warehouses[0]._id,
+                    priority: 2,
+                    is_active: false
                 },
                 {
-                    "_id": warehouses[1]._id,
-                    "priority": 0,
-                    "is_active": false
+                    _id: warehouses[1]._id,
+                    priority: 0,
+                    is_active: false
                 },
                 {
-                    "_id": warehouses[2]._id,
-                    "priority": 3,
-                    "is_active": false
+                    _id: warehouses[2]._id,
+                    priority: 3,
+                    is_active: false
                 }, {
-                    "_id": warehouses[3]._id,
-                    "priority": 1,
-                    "is_active": true
+                    _id: warehouses[3]._id,
+                    priority: 1,
+                    is_active: true
                 },
                 {
-                    "_id": warehouses[4]._id,
-                    "priority": 4,
-                    "is_active": false
+                    _id: warehouses[4]._id,
+                    priority: 4,
+                    is_active: false
                 }
             ],
             rp({
@@ -75,29 +74,28 @@ describe('Warehouse PUT API', () => {
                 expect(res.statusCode).toBe(200);
                 return models()['WarehouseTest'].find();
             }).then(res => {
-                console.log('->', res);
-
+            
                 expect(res.length).toBe(5);
 
                 // console.log( "properties", properties)
-                // res.forEach(element => {
-                //     const result=  properties.filter(o => !res.find(element => o.id === element.id))
-                //     // const result = properties.filter(item=>{item._id == element._id} );
-                //     console.log("results are:",result);
-                //     expect(element.priority).toBe(result[0].priority)
-                //     expect(element.is_active).toBe(result[0].is_active)
-                // });
+                res.forEach(x => {
+                    console.log(properties)
+                    let preProp = properties.find(y => y._id.toString() === x._id.toString())
+                    expect(preProp).toBeDefined();
+                    expect(x.priority).toBe(preProp.priority)
+                    expect(x.is_active).toBe(preProp.is_active)
+                });
 
-                expect(res[0].priority).toBe(2)
-                expect(res[0].is_active).toBe(false)
-                expect(res[1].priority).toBe(0)
-                expect(res[1].is_active).toBe(false)
-                expect(res[2].priority).toBe(3)
-                expect(res[2].is_active).toBe(false)
-                expect(res[3].priority).toBe(1)
-                expect(res[3].is_active).toBe(true)
-                expect(res[4].priority).toBe(4)
-                expect(res[4].is_active).toBe(false)
+                // expect(res[0].priority).toBe(2)
+                // expect(res[0].is_active).toBe(false)
+                // expect(res[1].priority).toBe(0)
+                // expect(res[1].is_active).toBe(false)
+                // expect(res[2].priority).toBe(3)
+                // expect(res[2].is_active).toBe(false)
+                // expect(res[3].priority).toBe(1)
+                // expect(res[3].is_active).toBe(true)
+                // expect(res[4].priority).toBe(4)
+                // expect(res[4].is_active).toBe(false)
 
                 done();
             })
