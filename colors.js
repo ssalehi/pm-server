a55127ad1cf64d9e3553b8859270ab05b62e7a5d
Qazal <jalilian.gh@gmail.com>
2018-11-26 11:26:54 +0330

    filtering undefined colors and adding them to dictionary for translation

--- colors.js
+++ colors.js
@@ -1,9 +1,5 @@
-const _const = require('./lib/const.list');
 const models = require('./mongo/models.mongo');
 const db = require('./mongo/index');
-var ColorModel = require('./lib/color.model');
-const copydir = require('copy-dir');
-const env = require('./env');
 var converter = require('css-color-converter');
 const Dictionary = require('./lib/dictionary.model')
 
@@ -12,52 +8,38 @@ let main = async () => {
 
 
   let res = await models()['Color'].find().lean();
-  console.log(res);
-
   let a = [];
   res.forEach(element => {
 
     cName = element.name
     a.push(safeColorConverter(cName))
   });
-
-  
   Promise.all(a).then(res => {
     process.exit(0);
-
   });
-
-
 }
-
-
 const colorConverter = function (cName) {
   return converter(cName.toLowerCase()).toHexString();
 };
-
 const safeColorConverter = function (cName) {
   if (cName) {
-    let words = cName.split(' ');
+    let words = cName.split(/\/| |-/);
+
     for (let i = 0; i < words.length; i++) {
       try {
         let cc = colorConverter(words[i]);
-        console.log('here is the conv', cc);
         return Promise.resolve(cc);
 
       } catch (e) {
-    
         body = {
-          name: cName,
-          value: 'SetPlease',
+          name: words[i],
+          value: 'undefined',
           type: 'color'
         }
-          return new Dictionary().addDictionary(this.body)
+        return new Dictionary().addDictionary(this.body)
       }
     }
   }
-
-  
-
 };
 
 main()
