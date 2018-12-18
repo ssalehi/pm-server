334e68858a6ad7602f88e45884e2ec20141a4aa0
305-2 <sareh@bentoak.systems>
2018-12-18 16:35:37 +0330

    end of a sad & painful story of using npm packages : node-rsa, and rsa-xml:((

--- routes/api.js
+++ routes/api.js
@@ -451,7 +451,7 @@ router.post('/placement/image/:pageId/:placementId', apiResponse('Page', 'addIma
 // checkout
 
 router.post('', apiResponse('Order', 'finalCheck', false, ['body']));
-router.post('/ipgpayment', apiResponse('Order', 'sendDataToBank', false, ['user','body']));
+router.post('/payment', apiResponse('Order', 'sendDataToBank', false, ['user','body']));
 router.post('/checkout', apiResponse('Order', 'checkoutCart', false, ['user', 'body.cartItems', 'body.order_id', 'body.address','body.transaction_id', 'body.used_point',
   'body.used_balance', 'body.total_amount', 'body.is_collect', 'body.discount', 'body.duration_days', 'body.time_slot', 'body.paymentType', 'body.loyalty']));
 router.post('/finalCheck', apiResponse('Order', 'finalCheck', false, ['body']));
