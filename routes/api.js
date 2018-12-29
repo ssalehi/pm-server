d0e24ab4d3a9b7f017d3c6d40a4bc7f1a8966615
305-2 <sareh@bentoak.systems>
2018-12-29 16:20:57 +0330

    demo

--- routes/api.js
+++ routes/api.js
@@ -453,6 +453,11 @@ router.post('/placement/image/:pageId/:placementId', apiResponse('Page', 'addIma
 router.post('', apiResponse('Order', 'finalCheck', false, ['body']));
 router.post('/payment', apiResponse('Order', 'prepareDataForBankGateway', false, ['user','body']));
 router.post('/payResult', apiResponse('Order', 'readAndVerifyPayResult', false, ['user','body']));
+
+router.post('/checkout', apiResponse('Order', 'checkoutCartDemo', false, ['user', 'body.cartItems', 'body.order_id', 'body.address','body.transaction_id', 'body.used_point',
+  'body.used_balance', 'body.total_amount', 'body.is_collect', 'body.discount', 'body.duration_days', 'body.time_slot', 'body.paymentType', 'body.loyalty']));
+
+
 router.post('/finalCheck', apiResponse('Order', 'finalCheck', false, ['body']));
 
 //sold out
