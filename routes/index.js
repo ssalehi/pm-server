b89f9aae948753561838864939b0f2ed1244aa7e
Amin Azarbadegan <amin@azarbadegan.net>
2019-01-26 14:59:18 +0330

    adding https (3rd attempt)

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,9 @@ router.use(function(req, res, next) {
 
 /* Diverting unknown routes to Angular router */
 router.all("*",function(req,res,next){
-  if(req.originalUrl.indexOf('api') === -1) {
+  if(req.get('Host').includes('lithium') && !req.secure) {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  } else if(req.originalUrl.indexOf('api') === -1) {
     console.log('[TRACE] Server 404 request: ' + req.originalUrl);
     const p = path.join(__dirname, '../public', 'index.html').replace(/\/routes\//, '/');
     res.status(200).sendFile(p);
