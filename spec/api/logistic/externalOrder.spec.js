2d0d52426f238f2c547119c5d0c2200a0e7c30e9
Qazal <jalilian.gh@gmail.com>
2019-01-26 16:14:08 +0330

    external order scan test

--- spec/api/logistic/externalOrder.spec.js
+++ spec/api/logistic/externalOrder.spec.js
@@ -59,29 +59,28 @@ describe('POST Order Ticket Scan-External Delivery', () => {
                     }
                 ],
                 instances: [{
-                        product_color_id: colorIds[0],
-                        size: "11",
-                        price: 2000,
-                        barcode: '0394081341',
-                        inventory: [{
-                            count: 3,
-                            reserved: 2,
-                            warehouse_id: warehouses[1]._id
-                        }, {
-                            count: 2,
-                            reserved: 0,
-                            warehouse_id: warehouses[2]._id
-                        }, {
-                            count: 3,
-                            reserved: 0,
-                            warehouse_id: warehouses[3]._id
-                        }, {
-                            count: 4,
-                            reserved: 0,
-                            warehouse_id: warehouses[4]._id
-                        }]
-                    }
-                ]
+                    product_color_id: colorIds[0],
+                    size: "11",
+                    price: 2000,
+                    barcode: '0394081341',
+                    inventory: [{
+                        count: 3,
+                        reserved: 2,
+                        warehouse_id: warehouses[1]._id
+                    }, {
+                        count: 2,
+                        reserved: 0,
+                        warehouse_id: warehouses[2]._id
+                    }, {
+                        count: 3,
+                        reserved: 0,
+                        warehouse_id: warehouses[3]._id
+                    }, {
+                        count: 4,
+                        reserved: 0,
+                        warehouse_id: warehouses[4]._id
+                    }]
+                }]
             }]);
 
             products = JSON.parse(JSON.stringify(products));
@@ -116,7 +115,7 @@ describe('POST Order Ticket Scan-External Delivery', () => {
                 },
                 { // customer 1: is_guest = true
                     is_guest: true,
-                    addresses: {
+                    addresses: [{
                         _id: mongoose.Types.ObjectId(),
                         province: "تهران",
                         city: "تهران",
@@ -134,7 +133,7 @@ describe('POST Order Ticket Scan-External Delivery', () => {
                             long: 51.379926,
                             lat: 35.696491
                         }
-                    },
+                    }],
                     active: true,
                     username: "qazaljl@gmail.com",
                     first_name: "qazal",
@@ -147,16 +146,26 @@ describe('POST Order Ticket Scan-External Delivery', () => {
             customer = JSON.parse(JSON.stringify(customer));
 
 
-            orders = await models()['OrderTest'].insertMany([{ // order 1 => a normal order which central warehosue has inventory for
-                customer_id: customer[0].id,
+            orders = await models()['OrderTest'].insertMany([{ // order 1 => a logged in customer order
+                customer_id: customer[0]._id,
+                address: customer[0].addresses[0],
                 order_time: new Date(),
+                delivery_info: {
+                    duration_days: 3,
+                    delivery_cost: 63000,
+                    delivery_discount: 0,
+                    delivery_expire_day: new Date(),
+                    time_slot: {
+                        lower_bound: 10,
+                        upper_bound: 14,
+                    }
+                },
                 is_cart: false,
                 tickets: [{
                     is_processed: false,
-                    _id: mongoose.Types.ObjectId(),
                     status: _const.ORDER_STATUS.ReadyToDeliver,
                     desc: null,
-                    receiver_id:warehouses.find(x => x.is_hub)._id,
+                    receiver_id: warehouses.find(x => x.is_hub)._id,
                     timestamp: new Date()
                 }],
                 order_lines: [{
@@ -168,13 +177,36 @@ describe('POST Order Ticket Scan-External Delivery', () => {
                     product_instance_id: products[0].instances[0]._id,
                     tickets: [{
                         is_processed: false,
-                        _id: warehouses.find(x => x.is_hub)._id,
                         status: _const.ORDER_LINE_STATUS.Delivered,
-                        receiver_id:warehouses.find(x => x.is_hub)._id,
+                        receiver_id: warehouses.find(x => x.is_hub)._id,
                         desc: null,
                         timestamp: new Date(),
                     }]
-                }, {
+                }]
+            },
+            { // order 2 => a guest customer order
+                customer_id: customer[1]._id,
+                address: customer[1].addresses[0],
+                order_time: new Date(),
+                delivery_info: {
+                    duration_days: 3,
+                    delivery_cost: 63000,
+                    delivery_discount: 0,
+                    delivery_expire_day: new Date(),
+                    time_slot: {
+                        lower_bound: 10,
+                        upper_bound: 14,
+                    }
+                },
+                is_cart: false,
+                tickets: [{
+                    is_processed: false,
+                    status: _const.ORDER_STATUS.ReadyToDeliver,
+                    desc: null,
+                    receiver_id: warehouses.find(x => x.is_hub)._id,
+                    timestamp: new Date()
+                }],
+                order_lines: [{
                     product_id: products[0]._id,
                     campaign_info: {
                         _id: mongoose.Types.ObjectId(),
@@ -183,47 +215,44 @@ describe('POST Order Ticket Scan-External Delivery', () => {
                     product_instance_id: products[0].instances[0]._id,
                     tickets: [{
                         is_processed: false,
-                        _id: warehouses.find(x => x.is_hub)._id,
-                        receiver_id:warehouses.find(x => x.is_hub)._id,
-
                         status: _const.ORDER_LINE_STATUS.Delivered,
+                        receiver_id: warehouses.find(x => x.is_hub)._id,
                         desc: null,
                         timestamp: new Date(),
                     }]
                 }]
-            }]);
+            }
+        
+        ]);
 
             orders = JSON.parse(JSON.stringify(orders));
+            // deliveries = await models()['DeliveryTest'].insertMany([{
+            //     to: {
+            //         customer: {
+            //             _id: customer[0].id,
+            //             address: customer[0].addresses[0]
+            //         }
+            //     },
+            //     from: {
+            //         warehouse_id: warehouses.find(x => x.is_hub)._id
 
-            deliveries = await models()['DeliveryTest'].insertMany([{
-                to: {
-                    customer: {
-                        id: customer[0].id,
-                        address: customer[0].addresses._id
-                    }
-                },
-                from: {
-                    warehouse_id: warehouses.find(x => x.is_hub)._id
-
-                },
-                order_details: [{
-                    order_line_ids: [
-                        orders[0].order_lines[1]._id,
-                    ],
-                    _id: mongoose.Types.ObjectId(),
-                    order_id: orders[0]._id
-                }],
-                start: new Date(),
-                tickets: [{
-                    is_processed: false,
-                    id: mongoose.Types.ObjectId(),
-                    status: _const.DELIVERY_STATUS.default,
-                    receiver_id: warehouses[3]._id,
-                    timestamp: new Date()
-                }],
-                "__v": 0
-            }]);
-            deliveries = JSON.parse(JSON.stringify(deliveries));
+            //     },
+            //     order_details: [{
+            //         order_line_ids: [
+            //             orders[0].order_lines[0]._id
+            //         ],
+            //         order_id: orders[0]._id
+            //     }],
+            //     start: new Date(),
+            //     tickets: [{
+            //         is_processed: false,
+            //         status: _const.DELIVERY_STATUS.default,
+            //         receiver_id: warehouses[3]._id,
+            //         timestamp: new Date()
+            //     }],
+            //     "__v": 0
+            // }]);
+            // deliveries = JSON.parse(JSON.stringify(deliveries));
             done();
         } catch (err) {
             console.log(err);
@@ -231,7 +260,7 @@ describe('POST Order Ticket Scan-External Delivery', () => {
     }, 15000);
 
 
-    it('should', async function (done) {
+    it('should scan product barcode and change its ticket to checked and initiates delivery with logged in customer', async function (done) {
         this.done = done
 
         const res = await rp({
@@ -241,19 +270,53 @@ describe('POST Order Ticket Scan-External Delivery', () => {
                 orderId: orders[0]._id,
                 barcode: '0394081341'
             },
-            user:{
-                warehouse_id: warehouses.find(x => x.is_hub)._id
+            method: 'POST',
+            json: true,
+            uri: lib.helpers.apiTestURL('order/ticket/scan'),
+            resolveWithFullResponse: true
+        });
+        expect(res.statusCode).toBe(200)
+        const deliveryData = await models()['DeliveryTest'].find()
+        const orderData = await models()['OrderTest'].find()
+        newOLTicketStatus = orderData[0].order_lines[0].tickets[orderData[0].order_lines[0].tickets.length - 1].status
+        expect(newOLTicketStatus).toBe(_const.ORDER_LINE_STATUS.Recieved)
+        expect(orderData[0].tickets[orderData[0].tickets.length - 1].status).toBe(_const.ORDER_STATUS.DeliverySet)
+        expect(deliveryData[0].tickets[deliveryData[0].tickets.length - 1].status).toBe(_const.DELIVERY_STATUS.default)
+        expect(JSON.stringify(deliveryData[0].order_details[0].order_line_ids[0])).toBe(JSON.stringify(orders[0].order_lines[0]._id))
+        done()
+
+
+    });
+
+
+    it('should scan product barcode and change its ticket to checked and initiates delivery with guest customer', async function (done) {
+        this.done = done
+
+        const res = await rp({
+            jar: hubClerk.jar,
+            body: {
+                trigger: _const.SCAN_TRIGGER.Inbox,
+                orderId: orders[0]._id,
+                barcode: '0394081341'
             },
             method: 'POST',
             json: true,
             uri: lib.helpers.apiTestURL('order/ticket/scan'),
             resolveWithFullResponse: true
         });
-
         expect(res.statusCode).toBe(200)
+        const deliveryData = await models()['DeliveryTest'].find()
+        const orderData = await models()['OrderTest'].find()
+        newOLTicketStatus = orderData[0].order_lines[0].tickets[orderData[0].order_lines[0].tickets.length - 1].status
+        expect(newOLTicketStatus).toBe(_const.ORDER_LINE_STATUS.Recieved)
+        expect(orderData[0].tickets[orderData[0].tickets.length - 1].status).toBe(_const.ORDER_STATUS.DeliverySet)
+        expect(deliveryData[0].tickets[deliveryData[0].tickets.length - 1].status).toBe(_const.DELIVERY_STATUS.default)
+        expect(JSON.stringify(deliveryData[0].order_details[0].order_line_ids[0])).toBe(JSON.stringify(orders[0].order_lines[0]._id))
         done()
 
 
     });
+
+
 
 });
