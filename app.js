ff2c6419139d5023f2577f226f51a6f683a66a92
Amin Azarbadegan <amin@azarbadegan.net>
2019-06-02 18:17:47 +0430

    Fixing vulnerabilities

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ let isReady = false;
 const passport = require('./passport');
 const session = require('./session');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger(env.isDev ? 'dev' : 'combined'));
