48d5b5af03dd3cff33eeadaacf9280f4af2d4c22
Ehsan Ansari <eabasir@gmail.com>
2019-10-23 14:09:59 +0330

    add update inventory

--- routes/api.js
+++ routes/api.js
@@ -258,6 +258,7 @@ router.post('/order/lost', apiResponse('TicketAction', 'lostReport', true, ['bod
 router.post('/order/offline/invoiceResponse', apiResponse('Offline', 'invoiceResponse', true, ['body'], [_const.ACCESS_LEVEL.OfflineSystem]));
 router.post('/order/offline/transferResponse', apiResponse('Offline', 'transferResponse', true, ['body'], [_const.ACCESS_LEVEL.OfflineSystem]));
 router.post('/order/offline/returnResponse', apiResponse('Offline', 'returnResponse', true, ['body'], [_const.ACCESS_LEVEL.OfflineSystem]));
+router.post('/order/offline/updateInventory', apiResponse('Offline', 'updateInventory', true, ['body'], [_const.ACCESS_LEVEL.OfflineSystem]));
 
 // offline reset order
 router.get('/order/offline/reset/:id', apiResponse('Offline', 'makeTestOrder', true, ['params.id'], [_const.ACCESS_LEVEL.OfflineSystem]));
